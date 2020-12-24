3b776019666a43715fe4b707331cae36993163b2
herowinb <herowinb@gmail.com>
2020-12-24 17:52:41 +0700

    Fix Metadata function not work and add demo remove title in auto.sh
    ffmpeg only work with PNG for attach_pic

--- config/auto.sh
+++ config/auto.sh
@@ -4,3 +4,5 @@
 #nohup /bin/bash /usr/src/app/live-dl https://www.youtube.com/channel/UCP0BspO_AMEe3aQqqpo89Dg &>/tmp/live-dl-moona.$(date +%d%b%y-%H%M%S).log &
 
 #nohup /bin/bash /usr/src/app/live-dl https://www.youtube.com/channel/UCOyYb1c43VlX9rc_lT6NKQw &>/tmp/live-dl-risu.$(date +%d%b%y-%H%M%S).log &
+
+#nohup /bin/bash /usr/src/app/live-dl -rt https://www.youtube.com/channel/UC1DCedRgGHBdm81E1llLhOQ &>/tmp/live-dl-pekora.$(date +%d%b%y-%H%M%S).log &
