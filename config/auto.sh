78211371c3bccb51d60e9e583f884edaa2df042e
herowinb <herowinb@gmail.com>
2021-01-27 11:35:07 +0700

    Add option long interval loop, check update

--- config/auto.sh
+++ config/auto.sh
@@ -3,7 +3,7 @@
 #remove # in below line, change URL channel and location/name log file (if need). Yes, you can add multi channel.
 #nohup /bin/bash /usr/src/app/live-dl -ac https://www.youtube.com/channel/UCP0BspO_AMEe3aQqqpo89Dg &>/tmp/live-dl-moona.$(date +%d%b%y-%H%M%S).log &
 
-#nohup /bin/bash /usr/src/app/live-dl -ac https://www.youtube.com/channel/UCP0BspO_AMEe3aQqqpo89Dg/community &>/tmp/live-dl-moona-membership.$(date +%d%b%y-%H%M%S).log &
+#nohup /bin/bash /usr/src/app/live-dl -li 900 https://www.youtube.com/channel/UCP0BspO_AMEe3aQqqpo89Dg/community &>/tmp/live-dl-moona-membership.$(date +%d%b%y-%H%M%S).log &
 
 #nohup /bin/bash /usr/src/app/live-dl https://www.youtube.com/channel/UCOyYb1c43VlX9rc_lT6NKQw &>/tmp/live-dl-risu.$(date +%d%b%y-%H%M%S).log &
 
