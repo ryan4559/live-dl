7a44cc97ba5ac2017b80ab7ad4afcb37d3973527
herowinb <herowinb@gmail.com>
2021-09-05 18:37:05 +0700

    New: Use ytarchive for Youtube Live download
    - Add new config and param to use ytarchive. If you not change config, Live-dl still use default Youtube-dl
    - Auto get last version when start docker (autoscript.sh)
    - Add new ext m4a for ytarchive audio_only format

--- autoscript.sh
+++ autoscript.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Auto install last version of youtube-dl and chat-downloader
 pip install --no-cache-dir --upgrade --quiet youtube-dl chat-downloader
+
+# Auto install last version of ytarchive
+curl -o /usr/src/app/ytarchive.py https://raw.githubusercontent.com/Kethsar/ytarchive/master/ytarchive.py
 
 bash /usr/src/app/config/auto.sh
 
