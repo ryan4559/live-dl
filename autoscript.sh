6a2d767088d55d52cbd61fb5fdf64e03e44587f9
herowinb <herowinb@gmail.com>
2021-11-21 22:42:09 +0700

    Update: New members stream detect method
    Youtube updated Membership tab with filtered member contents. So just pick first VideoID in this.

--- autoscript.sh
+++ autoscript.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Auto install last version of youtube-dl and chat-downloader
-pip install --no-cache-dir --upgrade --quiet youtube-dl chat-downloader
+# Auto install last version of youtube-dl, chat-downloader and twspace-dl for docker image
+pip install --no-cache-dir --upgrade --quiet youtube-dl chat-downloader twspace-dl
 
 # Auto install last version of ytarchive
 curl -o /usr/src/app/ytarchive.py https://raw.githubusercontent.com/Kethsar/ytarchive/master/ytarchive.py
 
+# Run main script
 bash /usr/src/app/config/auto.sh
 
+# Run Youtube-dl Web GUI
 python -u /usr/src/app/youtube-dl-server.py
